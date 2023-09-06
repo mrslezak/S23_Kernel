18b6a8bde0f99940ce4cdb009ac95e6c737b77d4
MattOfTheDead <matthew.slezak@gmail.com>
2023-09-05 20:24:41 -0500

    Fix making AnyKernel3 zip a valid flashable archive in TWRP

--- create_AK3_zip.sh
+++ create_AK3_zip.sh
@@ -3,5 +3,6 @@ cp out/msm---gki/dist/boot.img AnyKernel3
 cp out/msm---gki/dist/dtbo.img AnyKernel3
 cp out/msm---gki/dist/vendor_dlkm.img AnyKernel3
 cp out/msm---gki/dist/vendor_boot.img AnyKernel3
-zip -r8 kernel_installer.zip AnyKernel3/
-
+cd AnyKernel3
+zip -r8 kernel_installer.zip .
+mv kernel_installer.zip ../
